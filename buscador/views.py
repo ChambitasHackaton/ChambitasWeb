e77d789d36eb994bb44a6a7b20b4f11d4f776e0d
Alan Sanchez <sanchezpineda03@gmail.com>
2015-10-04 01:35:30 -0500

    Buscador terminado

--- buscador/views.py
+++ buscador/views.py
@@ -1,23 +1,27 @@
 from usuarios.models import MyUser
 from oficios.models import Oficio
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.template import Context, loader
 
-def search_results(request):
-	parameters = {
-		'oficio': request.GET.get('oficio'),
-		'delegacion': request.GET.get('delegacion'),
-		'codigo_postal': request.GET.get('cp'),
-		'oficio': request.GET.get('oficio'),
-	}
-	oficio = Oficio.objects.all().filter(oficio = parameters['oficio'])
-	usuarios = MyUser.objects.all().filter(oficio = oficio,
-											zip_code = parameters['codigo_postal'],
-											delegacion = parameters['delegacion'],)
-	print(usuarios)
-	if not request.GET.get('aa'):
-		print('ups')
-	else:
-		print('yey')
-	template = loader.get_template('test.html')
-	return HttpResponse(template.render())
+class SearchResults(TemplateView):
+	template_name = 'test.html'
+	
+	def get_context_data(self, **kwargs):
+		context = super(SearchResults, self).get_context_data(**kwargs)
+		parameters = {
+			'delegacion': self.request.GET.get('delegacion').replace('-', ' ').title(),
+			'zip_code': self.request.GET.get('cp'),
+			'oficio': self.request.GET.get('oficio').upper().replace('-', ' '),
+		}
+		print(parameters['delegacion'])
+		try:
+			oficio = Oficio.objects.only('id').get(oficio = parameters['oficio']).id
+			usuarios = MyUser.objects.all().filter(
+				oficio = oficio,
+				delegacion = parameters['delegacion'],
+				zip_code = parameters['zip_code'],
+			)
+		except:
+			usuarios = []
+		context['usuarios'] = usuarios
+		return context
